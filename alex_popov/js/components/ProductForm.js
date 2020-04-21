9abeb7b5a75ddef73682858a92ab556d332bd357
Alex Popov <letmebetomorrow@mail.ru>
2020-04-21 16:33:57 +0300

    productForm class commented

--- alex_popov/js/components/ProductForm.js
+++ alex_popov/js/components/ProductForm.js
@@ -1,30 +1,32 @@
-
+// this class works with form that used for entering and editingg information about a product
+// constructor recivew characters of currenct and delimiter
+// action that used for daving new information and and object with information about a product
+// if object wasn't passed idt has empty abject
+// this situation can happen when user want to add a new product
 class ProductForm extends Application {
     constructor(currency, priceDelimiter, action, productInformation = {'name': '', 'email': '', 'count': 0, 'price': 0, 'delivery': {}}) {
         super();
-        // this.on()
+        
         this.currency = currency;
         this.priceDelimiter = priceDelimiter;
         this.action = action;
         this.productInformation = productInformation;
-
+        // regular expressions for checking data
         this.validation = {
             'name': /^[\w]{1,}@[a-z]{1,}.[a-z]{2,}$/,
             'email': /^[\w-_]{1,}[@]{1}[a-z-_]{1,}[.]{1}[a-z]{2,}$/,
             'number': /^\d{1,}$/,
             'price': /^[0-9]*[.]?[0-9]{0,2}$/
         }
-
+        // binding functions that work with data about product
         this.onInputName = this.onInputName.bind(this);
         this.onInputEmail = this.onInputEmail.bind(this);
         this.onInputNumber = this.onInputNumber.bind(this);
         this.onChangePrice = this.onChangePrice.bind(this);
         this.onClickPrice = this.onClickPrice.bind(this);
-        
-        // this.off = this.off.bind(this);
-
-        this.cityAdder = this.cityAdder.bind(this);
-
+        this.productEditer = this.productEditer.bind(this);
+        // structures of elements
+        // used for randering information about delivery for selectiong cities and countries
         this.structures = {
             select: `<option value="choose">choose</option>
             <% list.forEach( (el, index) => { %>
@@ -44,121 +46,86 @@ class ProductForm extends Application {
         }
     }
 
-    formHighliter(bool, el) {
-        // const el = $(selector);
-        if ( bool ) {
-            el.addClass('is-valid')
-            el.removeClass('is-invalid')
-        } else {
-            el.addClass('is-invalid')
-            el.removeClass('is-valid')
-        }
-    }
 
-    invalidFieldDetected(id) {
-        $( id ).focus();
-        $( id ).addClass('is-invalid')
-    }
 
     deliveryCreator() {
+        // compounnd information about cities that are avialible for delivery
+        // looks for checked checboxes and makes the object
         const delivery = {}
         $('#deliveryCities').find('input:checked').each( (i, el) => {
-            const data = el.id.split('-')
-            // console.log(!( data[0] in dd) )
+            const data = el.id.split('-');
             if (! (data[0] in delivery) ) {
-                // console.log('create')
-                delivery[data[0]] = []
+                delivery[data[0]] = [];
             }
-            delivery[data[0]].push(data[1])
+            delivery[data[0]].push(data[1]);
         })
 
         return delivery;
     }
 
-    cityAdder(ev) {
-        ev.preventDefault()
-
-        if (this.productInformation.name === '' || this.productInformation.name.length < 5 || this.productInformation.name.length > 15) {
-            this.invalidFieldDetected('#name')
-        } else if ( !this.validation.email.test( this.productInformation.email ) ) {
-            // console.log(this.validation.email.test( this.productInformation.email) )
-            // console.log('eamail failed')
-            this.invalidFieldDetected('#email')
-            // $('#email').focus()
-            // $('#email').addClass('is-invalid')
-        } else if (this.productInformation.count === 0) {
-            // console.log('count failed')
-            this.invalidFieldDetected('#count')
-            // $('#count').focus()
-            // $('#count').addClass('is-invalid')
-        } else if (this.productInformation.price === 0) {
-            // console.log('price failed')
-            this.invalidFieldDetected('#price')
+    formHighliter(bool, el) {
+        // gives error class for an imput if it was checed as correct or incorrect
+        if ( bool ) {
+            el.addClass('is-valid');
+            el.removeClass('is-invalid');
         } else {
-            console.log('everuthing is ok')
-            this.productInformation.delivery = this.deliveryCreator();
-            console.log(this.productInformation)
-            this.action(this.productInformation)
-            this.off(ev)
-            // console.log(this)
+            el.addClass('is-invalid');
+            el.removeClass('is-valid');
         }
-
-
-        //  // this.productInformation.dilivery[data[0]] = data[1]
-    }
-
-    // onSubmit = (ev) => {
-    //     ev.preventDefault()
-    //     // console.log('asdf' in this.productInformation.delivery)
-    //     // console.log( $('#deliveryCities').find('input:checked') )
-    //     // const delivery = {}
-    //     const dd
-    //     $('#deliveryCities').find('input:checked').each( this.cityAdder)
-    //     // console.log(this.productInformation.dilivery)
-    // }
-
-    onClickAll() {
-        // console.log(this)
-        const selectedCountry = $('#countriesSelector').val();
-        console.log( $('#selectAll').prop("checked"))
-        
-        if (selectedCountry !== 'choose'){
-            $(`[data-country="${selectedCountry}"]` ).find('input').prop('checked', $('#selectAll').prop("checked"))
-        }
-    }
-
-    onInputName() {
-        
-        this.productInformation.name = String( $('#name').val() ).trim();
-        
-        if (this.productInformation.name.length > 15) {
-            this.productInformation.name = this.productInformation.name.slice(0, -1);
-            $('#name').val(this.productInformation.name)
-        }
-
-        this.formHighliter(this.productInformation.name.length > 4, $('#name') )
-    }
-
-    onInputEmail() {
-        
-        const el = $('#email')
-        const isEmailValid = this.validation.email.test( el.val().toLowerCase() );
-        // console.log(isEmailValid)
-
-        this.formHighliter(isEmailValid, $('#email'))
-
-        this.productInformation.email = $('#email').val()
-        
     }
 
     InputZeroValue(id, newVal) {
-        // this.productInformation[id] = newVal;
-        $(`#${id}`).val(newVal)
+        // I supposed if the price or count values get incoret value
+        // it stores 0 if every simbols are not a digit
+        // and if price got before 100 and then got 'f/ for example
+        // it should store 100 and miss 'f' after 100
+        $(`#${id}`).val(newVal);
         if (newVal === '') {
             newVal = 0;
             this.formHighliter(false, $(`#${id}`) )
         }
         this.productInformation[id] = newVal;
+    }
+
+    invalidFieldDetected(id) {
+        // if there are incorrect values in the form 
+        // if gives the focus to the input and error class
+        $( id ).focus();
+        $( id ).addClass('is-invalid');
+    }    
+
+    onClickAll() {
+        // makes all the checkbox of cities checed or unchecked in the container with chosen country
+        const selectedCountry = $('#countriesSelector').val();
+        
+        if (selectedCountry !== 'choose'){
+            $(`[data-country="${selectedCountry}"]` ).find('input').prop('checked', $('#selectAll').prop("checked"));
+        }
+    }
+
+    onInputName() {
+        // checks the name value of the prosuct if the number of caharacters in the name value
+        // less tham 5 the input got is-invalid class
+        // user cant enter more than 15 charecters for the name value
+        // so we keep users of annoying situations with filling the form
+        this.productInformation.name = String( $('#name').val() ).trim();
+        
+        if (this.productInformation.name.length > 15) {
+            this.productInformation.name = this.productInformation.name.slice(0, -1);
+            $('#name').val(this.productInformation.name);
+        }
+
+        this.formHighliter(this.productInformation.name.length > 4, $('#name') );
+    }
+
+    onInputEmail() {
+        // saves email value into store and give the input error class
+        // I suppesed that  this is only one value that can be incorrect in the sotre
+        const el = $('#email')
+        const isEmailValid = this.validation.email.test( el.val().toLowerCase() );
+        this.productInformation.email = $('#email').val();
+
+        this.formHighliter(isEmailValid, $('#email'));
     }
 
     onInputNumber(ev) {
@@ -176,133 +143,155 @@ class ProductForm extends Application {
 
         if (count !== '') {
             if (reg.test(count)) {
-                // console.log(count)
-                const number = Number( (+count).toFixed(2) )
+
+                const number = Number( (+count).toFixed(2) );
                 this.productInformation[elementId] = number;
-                this.formHighliter(true, $(`#${elementId}`))
+                this.formHighliter(true, $(`#${elementId}`));
                 
             } else {
-                // console.log('false')
+                
                 if (elementId === 'price') {
-                    // console.log(1)
-                    count = count.replace(reg2, '')
-                    count = Number( (+count).toFixed(2) ) 
+                    count = count.replace(reg2, '');
+                    count = Number( (+count).toFixed(2) ) ;
                 } else if (elementId === 'count') {
-                    // console.log(2)
-                    count = Number( count.replace(reg2, '') )
+                    
+                    count = Number( count.replace(reg2, '') );
                 }
-                // console.log(count)
-                this.InputZeroValue(elementId, count)
-                // console.log(count)
+                
+                this.InputZeroValue(elementId, count);
+                
                 if (count === 0) {
-                    this.formHighliter(false, $(`#${elementId}`))
+                    this.formHighliter(false, $(`#${elementId}`));
                 }
             }
 
         } else {
-            this.InputZeroValue(elementId, '')
-            this.formHighliter(false, $(`#${elementId}`))
+            this.InputZeroValue(elementId, '');
+            this.formHighliter(false, $(`#${elementId}`));
         }
     }
 
     onChangePrice(ev) {
-        
-        const val = ev.target.value.trim()
+        // shows price as number if the element loose focus
+        // $ 10,000.00 insted of 10000
+        const val = ev.target.value.trim();
         if (val !== '') {
-            ev.target.value = this.priceMaker(val, this.currency, this.priceDelimiter)
+            ev.target.value = this.priceMaker(val, this.currency, this.priceDelimiter);
         }
     }
 
     onClickPrice(ev) {
-        // console.log('click price')
-        // console.log(ev.target.value)
-        // console.log(this.productInformation.price)
+        // shows price as number if the element was clicked
+        // 10000 insted of $ 10,000.00
         if (this.productInformation && ev.target.value) {
-            console.log('change')
             ev.target.value = this.productInformation.price;
         }
     }
 
     onSelectCountry() {
+        // dispay the container that contains the cities of selected country
+        // activates or disactivates select all cities element if it is necessary
+        // if all the cities were checked selecet all wil be checked
         if (this.value !== 'choose') {
-            $('#selectAll').attr('disabled', false)
-            $('.countryCities').addClass('displayNone')
-            $(`[data-country="${this.value}"]`).removeClass('displayNone')
+            $('#selectAll').attr('disabled', false);
+            $('.countryCities').addClass('displayNone');
+            $(`[data-country="${this.value}"]`).removeClass('displayNone');
         
             const inputs = $(`[data-country="${this.value}"]` ).find('input');
             let selected = 0;
             inputs.each( (i, el) => {
-                // console.log( $(el).prop('checked') )
+
                 if ( $(el).prop('checked') === false) {
-                    // console.log('false')
-                    $('#selectAll').prop('checked', false)
+                    $('#selectAll').prop('checked', false);
                 } else {
-                    selected++
+                    selected++;
                 }
             })
 
             if (selected === inputs.length) {
-                $('#selectAll').prop('checked', true)
+                $('#selectAll').prop('checked', true);
             }
 
         } else {
-            $('#selectAll').attr('disabled', true)
-            $('#selectAll').prop('checked', false)
-            $('.countryCities').addClass('displayNone')
+            $('#selectAll').attr('disabled', true);
+            $('#selectAll').prop('checked', false);
+            $('.countryCities').addClass('displayNone');
         }
     }
 
     on() {
+        // adds events and displays form and darker
         $('#an-container').removeClass('displayNone');
-        $('.darker').removeClass('displayNone')
+        $('.darker').removeClass('displayNone');
 
         $('#formCancel').on('click', this.off);
-        $('#formSubmit').on('click', this.cityAdder);
+        $('#formSubmit').on('click', this.productEditer);
         $('#selectAll').on('click', this.onClickAll);
         $('#countriesSelector').on('change', this.onSelectCountry);
 
-        $('#name').on('input', this.onInputName)
-        $('#email').on('input', this.onInputEmail)
-        $('#count').on('input', this.onInputNumber)
-        $('#price').on('input', this.onInputNumber)
-        $('#price').on('focusout', this.onChangePrice)
-        $('#price').on('click', this.onClickPrice)
-
-        // $('#deliveryCities').on('click', this.onClickCities)
+        $('#name').on('input', this.onInputName);
+        $('#email').on('input', this.onInputEmail);
+        $('#count').on('input', this.onInputNumber);
+        $('#price').on('input', this.onInputNumber);
+        $('#price').on('focusout', this.onChangePrice);
+        $('#price').on('click', this.onClickPrice);
     }
 
     off(ev) {
+        //removes events and hide the form and darker
         ev.preventDefault()
-        // this.offActions()
-        $('#formCancel').off('click', this.onCancelform)
-        $('#formSubmit').off('click', this.onSubmitForm)
-        $('#selectAll').off('click', this.onClickAll)
+
+        $('#formCancel').off('click', this.onCancelform);
+        $('#formSubmit').off('click', this.onSubmitForm);
+        $('#selectAll').off('click', this.onClickAll);
         $('#countriesSelector').off('change', this.onSelectCountry);
 
-        $('#name').off('input', this.onInputName)
-        $('#email').off('input', this.onInputEmail)
-        $('#count').off('input', this.onInputNumber)
-        $('#price').off('input', this.onInputNumber)
-        $('#price').off('focusout', this.onChangePrice)
-        $('#price').off('click', this.onClickPrice)
-        // $('#deliveryCities').off('click', this.onClickCities)
+        $('#name').off('input', this.onInputName);
+        $('#email').off('input', this.onInputEmail);
+        $('#count').off('input', this.onInputNumber);
+        $('#price').off('input', this.onInputNumber);
+        $('#price').off('focusout', this.onChangePrice);
+        $('#price').off('click', this.onClickPrice);
 
         $('#selectAll').prop('checked', false);
 
         $('#an-container').addClass('displayNone');
         $('.darker').addClass('displayNone')
 
-        $('#addNewForm').find('input').val('')
-        $('#addNewForm').find('input').removeClass('is-valid', 'is-invalid')
-        $('#addNewForm').find('input').removeClass('is-invalid')
+        $('#addNewForm').find('input').val('');
+        $('#addNewForm').find('input').removeClass('is-valid', 'is-invalid');
+        $('#addNewForm').find('input').removeClass('is-invalid');
+    }
+
+    productEditer(ev) {
+        // check the whol form. if a value is not correct this input get focus
+        // cif everylthing is ok compound  delivery informatin and saves the product
+        ev.preventDefault();
+
+        if (this.productInformation.name === '' || this.productInformation.name.length < 5 || this.productInformation.name.length > 15) {
+            this.invalidFieldDetected('#name');
+        } else if ( !this.validation.email.test( this.productInformation.email ) ) {
+            this.invalidFieldDetected('#email');
+        } else if (this.productInformation.count === 0) {
+            this.invalidFieldDetected('#count');
+        } else if (this.productInformation.price === 0) {
+            this.invalidFieldDetected('#price');
+        } else {
+            this.productInformation.delivery = this.deliveryCreator();
+            this.action(this.productInformation);
+            this.off(ev);
+        }
     }
 
     render(deliveryIformation) {
+        // renders countries as options of the select element of the form
+        // and renders cities as checkboxes
         const countries = Object.keys(deliveryIformation);
-        super.render(this.structures.cities, deliveryIformation, '#deliveryCities' )
+
+        super.render(this.structures.cities, deliveryIformation, '#deliveryCities' );
         super.render(this.structures.select, countries, '#countriesSelector');
-    
-        this.on()
+        // adds events
+        this.on();
     }
 
 }
